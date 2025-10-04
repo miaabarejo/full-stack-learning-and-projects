f32e6031a4a1001a1303201b243edbfe1c7d70d7
Mia Abarejo <miaabarejo48@gmail.com>
2025-10-04 13:29:59 +0800

    Add files via upload

---
+++ 01-front-end-web-development/19-jquery/index.js
@@ -0,0 +1,60 @@
+//behavior
+
+// $("h1").addClass("big-title margin-50");
+
+//$("h1").text("Bye");
+//
+// $("button").text("Don't Click Me");
+//
+// $("button").html("<em>Hey</em>");
+//
+// $("a").attr("href", "https://www.yahoo.com");
+
+//Click
+// $("h1").click(function() {
+//   $("h1").css("color", "purple");
+// });
+
+// event listener VANILLA JS
+// for (var i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function() {
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+// //event listener jquery
+// $("button").click(function() {
+//   $("h1").css("color", "purple");
+// });
+
+// $("input").keypress(function(event) {
+//   console.log(event.key);
+// });
+
+// drum kit
+// $(document).keypress(function(event) {
+//   console.log(event.key);
+// });
+
+// $(document).keypress(function(event) {
+//   $("h1").text(event.key);
+// });
+
+// more flexible than keypress, on
+// $("h1").on("mouseover", function() {
+//   $("h1").css("color", "purple");
+// });
+
+// you can also do click rather than mouseover
+// $("h1").on("click", function() {
+//   $("h1").css("color", "purple");
+// });
+
+// insert button
+// $("h1").before("<button>New</button>");
+// $("h1").after("<button>New</button>");
+// $("h1").prepend("<button>New</button>");
+// $("h1").append("<button>New</button>");
+
+// removing buttons console.log
+// $("button").remove();
